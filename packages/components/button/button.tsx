72c292d600616cc9c2b01e265eb07aee950917e3
凤之兮原 <1577310596@qq.com>
2023-04-21 22:23:16 +0800

    feat: finish first version about css

--- packages/components/button/button.tsx
+++ packages/components/button/button.tsx
@@ -1,31 +1,50 @@
 import React from 'react'
 import classNames from 'classnames'
-import { ButtonProps, CompoundedComponent } from './type';
+import { ButtonProps, CompoundedComponent } from './type'
 
+const InternalButton: React.ForwardRefRenderFunction<
+	HTMLButtonElement,
+	ButtonProps
+> = (props, ref) => {
+	const {
+		type,
+		className,
+		children,
+		htmlType = 'button',
+		...rest
+	} = props
 
-const InternalButton: React.ForwardRefRenderFunction<HTMLButtonElement, ButtonProps> = (props, ref) => {
-  const { type, className, children, htmlType = 'button' } = props
+	// 通过 rest 获取key => value
+	// const getStyle = () => {
+	//   const style: string[] = []
+	//   Object.keys(rest).forEach(key => {
+	//     style.push(`${key}-${rest[key]}`)
+	//   })
+	//   return style
+	// }
+	// console.log(getStyle());
 
-  const classes = classNames('btn', {
-    [`kylin-btn-${type}`]: type,
-  }, className)
+	const classes = classNames(
+		'btn',
+		{
+			[`kylin-btn-${type}`]: type,
+		},
+		className
+	)
 
+	let buttonNode = (
+		<button type={htmlType} className={classes} {...rest}>
+			{children}
+		</button>
+	)
 
-  let buttonNode = (
-    <button
-      type={htmlType}
-      className={classes}
-    >
-      {children}
-    </button>
-  )
-
-  return buttonNode
+	return buttonNode
 }
 
-const Button = React.forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
-  InternalButton
-) as CompoundedComponent
+const Button = React.forwardRef<
+	HTMLButtonElement | HTMLAnchorElement,
+	ButtonProps
+>(InternalButton) as CompoundedComponent
 
 Button.__KYLIN_BUTTON = true
 
--- packages/components/button/demo/button-type.tsx
+++ packages/components/button/demo/button-type.tsx
@@ -3,8 +3,8 @@ import Button from "../button";
 const ButtonType = () => {
   return (
     <>
-      <Button type="primary" className="text-primary">123123</Button>
-      <h1>123123</h1>
+      <Button type="primary" className="text-primary" bg="blue-500" font='bold' text='white'>123123</Button>
+      <h1 className="bg-blue-500">123123</h1>
     </>
   );
 }
