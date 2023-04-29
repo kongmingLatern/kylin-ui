e7e01241a0837823f744af23973ba5ee267ba8a2
凤之兮原 <1577310596@qq.com>
2023-04-29 22:26:25 +0800

    feat: add component build

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import '@unocss/reset/normalize.css'
 import '../packages/theme/src/style/index.scss'
+import Button from '../dist/index.es'
 import 'uno.css'
 
 ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 ).render(
 	<React.StrictMode>
-		<App />
+		<Button type="primary" bg="purple-500" color="red">
+			123123
+		</Button>
 	</React.StrictMode>
 )
