038ec65e0ef063d3f2035cdf01ba116d9e3a8b18
凤之兮原 <1577310596@qq.com>
2023-04-29 22:45:54 +0800

    refactor: refactor build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import json from '@rollup/plugin-json'
 
 import Unocss from 'unocss/vite'
 import react from '@vitejs/plugin-react'
+
+import {
+	BuildLib,
+	BuildPlugins,
+	rollupOptions,
+} from './scripts/build'
 
 export default defineConfig({
 	plugins: [
@@ -15,32 +15,17 @@ export default defineConfig({
 		Unocss({
 			configFile: './uno.config.ts',
 		}),
-		reactRefresh(),
-		tsconfigPaths(),
-		peerDepsExternal(),
-		commonjs(),
-		resolve(),
-		json(),
+		...BuildPlugins(),
 	],
 	build: {
 		outDir: 'dist',
 		sourcemap: true,
 		minify: 'esbuild',
-
 		lib: {
-			entry: './packages/components/button/button.tsx',
-			formats: ['es', 'umd', 'iife'],
-			name: 'KylinUI',
-			fileName: format => `index.${format}.js`,
+			...BuildLib(),
 		},
 		rollupOptions: {
-			external: ['react', 'react-dom'],
-			output: {
-				globals: {
-					react: 'React',
-					'react-dom': 'ReactDOM',
-				},
-			},
+			...rollupOptions(),
 		},
 	},
 })
