c471a8132c6cc335fe2c8ef3bd1522d3e87d1381
凤之兮原 <1577310596@qq.com>
2023-04-30 20:20:16 +0800

    chore: add workflow for lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
 import '@unocss/reset/normalize.css'
 import '../packages/theme/src/style/index.scss'
 import Button from '../dist/index.es'
