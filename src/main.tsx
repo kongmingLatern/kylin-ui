de54b1d4fdea135636fe796c3da90ecb74a7ad65
凤之兮原 <1577310596@qq.com>
2023-05-01 19:57:08 +0800

    chore: add eslint config

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,11 @@ import Button from '../dist/index.es'
 import 'uno.css'
 
 ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement
 ).render(
 	<React.StrictMode>
 		<Button type="primary" bg="purple-500" color="red">
-			123123
+      123123
 		</Button>
 	</React.StrictMode>
 )
