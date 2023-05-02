020b5055930d8c7a04d7cf3248b3c6794c64b543
凤之兮原 <1577310596@qq.com>
2023-05-02 20:56:02 +0800

    chore: add demo for button

--- packages/components/button/demo/button-type.tsx
+++ packages/components/button/demo/button-type.tsx
@@ -1,12 +1,24 @@
 import Button from '../button'
 
 const ButtonType = () => {
-	return (
-		<>
-			<Button type="primary">123123</Button>
-			<Button type="secondary">123123</Button>
-		</>
-	)
+  return (
+    <>
+      <Button type="primary">123123</Button>
+      <Button type="secondary">123123</Button>
+
+      {/* ==================== Props ==================== */}
+      {/* TODO: */}
+      {/* <Button
+        type="primary"
+        disabled
+        icon={'icon'}
+        loading={true}
+        href={'www.baidu.com'}
+        shape='circle'
+        value={'value'}
+      /> */}
+    </>
+  )
 }
 
 export default ButtonType
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
+import App from './App'
 import '@unocss/reset/normalize.css'
 import '../packages/theme/src/style/index.scss'
-import Button from '../dist/index.es'
 import 'uno.css'
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
-	<React.StrictMode>
-		<Button type="primary" bg="purple-500" color="red">
-      123123
-		</Button>
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
