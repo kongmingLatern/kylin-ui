4c1574feb2e65695f1bafd623e469b3e2355b369
凤之兮原 <1577310596@qq.com>
2023-05-06 22:07:24 +0800

    chore: clear

--- packages/components/Flex/Flex.tsx
+++
@@ -1,9 +0,0 @@
-const Flex = () => {
-      return (
-        <div>
-          <h1>Flex Component</h1>
-        </div>
-      );
-    };
-
-    export default Flex;
--- packages/components/Flex/__tests__/Flex.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Flex from '../Flex';
-test('renders Flex component', () => {
-  render(<Flex />);
-  const linkElement = screen.getByText(/Flex Component/i);
-expect(linkElement.firstChild).toMatchInlineSnapshot('Flex Component')
-});
