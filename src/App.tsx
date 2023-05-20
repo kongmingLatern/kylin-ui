ce50352a4e4a0348c870b66dc389d85eaf20abb2
凤之兮原 <1577310596@qq.com>
2023-05-20 21:44:44 +0800

    refactor: refactor button animation css

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ import ThemeButton from '../packages/components/ThemeProvider/demo/button-theme'
 import CommonButton from '../packages/components/button/demo/type'
 
 const App = () => {
-  return (
-    <div>
-      <h1>Type Button</h1>
-      <CommonButton />
-      <h1>Theme Button</h1>
-      <br />
-      <ThemeButton />
+	return (
+		<div>
+			<h1>Type Button</h1>
+			<CommonButton />
+			<h1>Theme Button</h1>
+			<br />
+			<ThemeButton />
 
-      <Button bg="red-500">123</Button>
-      <Button bg="blue-500">123</Button>
-      <Button bg="green-500">123</Button>
-    </div>
-  )
+			<Button bg="red-500">123</Button>
+			<Button bg="blue-500">123</Button>
+			<Button bg="green-500">123</Button>
+		</div>
+	)
 }
 
 export default App
