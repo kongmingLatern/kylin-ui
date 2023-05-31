4709819b1e75d6d8ca0e001f0ee533b1c49c2a8c
凤之兮原 <1577310596@qq.com>
2023-05-31 21:52:08 +0800

    chore: add paragraph demo for app

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,32 @@ import ThemeButton from '@packages/components/ThemeProvider/demo/button-theme'
 import CommonButton from '@components/Button/demo/button'
 import { ButtonGroupDemo } from '@components/Button/demo/button-group'
 import { TitleDemo } from '@components/Typography/demo/Title'
+import { TextDemo } from '@components/Typography/demo/Text'
+import { ParagraphDemo } from '@components/Typography/demo/Paragraph'
+import { Title } from '@components/Typography'
 
 const App = () => {
-	return (
-		<>
-			<TitleDemo />
-			<br />
-			<CommonButton />
-			<br />
-			<ButtonGroupDemo />
-			<br />
-			<ThemeButton />
-			<br />
-		</>
-	)
+  return (
+    <>
+      <Title mark>ParagraphDemo</Title>
+      <ParagraphDemo />
+      <br />
+      <Title mark>TextDemo</Title>
+      <TextDemo />
+      <br />
+      <Title mark>TitleDemo</Title>
+      <TitleDemo />
+      <br />
+      <Title mark>ButtonDemo</Title>
+      <CommonButton />
+      <br />
+      <Title mark>ButtonGroupDemo</Title>
+      <ButtonGroupDemo />
+      <br />
+      <ThemeButton />
+      <br />
+    </>
+  )
 }
 
 export default App
