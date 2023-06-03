6b88603924fee67968aafa67666a02feab4a8ca0
凤之兮原 <1577310596@qq.com>
2023-06-03 22:51:09 +0800

    test: add test cases

--- packages/components/Space/__tests__/space.spec.tsx
+++ packages/components/Space/__tests__/space.spec.tsx
@@ -77,4 +77,103 @@ describe('props', () => {
     `
     )
   })
+  test('align', () => {
+    const { container } = render(
+      <Space align="end">
+        <div>first</div>
+        <div>secord</div>
+      </Space>
+    )
+    expect(container.firstChild).toMatchInlineSnapshot(
+      `
+      <div
+        class="kylin-space-align-end"
+      >
+        <div
+          class="kylin-space-item"
+          style="margin-left: 8px;"
+        >
+          <div>
+            first
+          </div>
+        </div>
+        <div
+          class="kylin-space-item"
+        >
+          <div>
+            secord
+          </div>
+        </div>
+      </div>
+    `
+    )
+  })
+  test('split', () => {
+    const { container } = render(
+      <Space split>
+        <div>first</div>
+        <div>secord</div>
+      </Space>
+    )
+    expect(container.firstChild).toMatchInlineSnapshot(
+      `
+      <div
+        class="kylin-space-align-center"
+      >
+        <div
+          class="kylin-space-item"
+          style="margin-left: 4px;"
+        >
+          <div>
+            first
+          </div>
+        </div>
+        <span
+          class="kylin-space-item-split"
+          style="margin-left: 4px;"
+        />
+        <div
+          class="kylin-space-item"
+        >
+          <div>
+            secord
+          </div>
+        </div>
+      </div>
+    `
+    )
+  })
+  test('wrap', () => {
+    const { container } = render(
+      <Space wrap>
+        <div>first</div>
+        <div>secord</div>
+      </Space>
+    )
+    expect(container.firstChild).toMatchInlineSnapshot(
+      `
+      <div
+        class="kylin-space-align-center"
+        style="flex-wrap: wrap;"
+      >
+        <div
+          class="kylin-space-item"
+          style="margin-left: 8px; padding-bottom: 8px;"
+        >
+          <div>
+            first
+          </div>
+        </div>
+        <div
+          class="kylin-space-item"
+          style="padding-bottom: 8px;"
+        >
+          <div>
+            secord
+          </div>
+        </div>
+      </div>
+    `
+    )
+  })
 })
