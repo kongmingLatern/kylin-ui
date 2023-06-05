4c829f560438cf800a6bf5faaa71af039e455489
凤之兮原 <1577310596@qq.com>
2023-06-05 22:37:21 +0800

    feat: add Avatar component

---
+++ packages/components/Avatar/Avatar.tsx
@@ -0,0 +1,85 @@
+import classNames from 'classnames'
+import React from 'react'
+
+interface AvatarProps {
+  src?: string
+  alt?: string
+  size?: number
+  shape?: 'circle' | 'square'
+  draggable?: boolean | 'true' | 'false'
+  className?: string
+  style?: React.CSSProperties
+  icon?: React.ReactNode
+  children?: React.ReactNode
+}
+
+const InternalAvatar: React.ForwardRefRenderFunction<
+  HTMLImageElement,
+  AvatarProps
+> = (props, ref) => {
+  const {
+    src,
+    alt,
+    icon,
+    size = 32,
+    shape = 'circle',
+    draggable,
+    className,
+    style,
+    children,
+    ...rest
+  } = props
+
+  const isImgExist = src && src.length > 0
+
+  if (isImgExist && React.isValidElement(children)) {
+    console.warn(
+      'Avatar',
+      `Avatar children and src can not exist at same time.`
+    )
+  }
+
+  const classes = classNames(
+    {
+      [`kylin-avatar-${shape}`]: !!shape,
+      [`kylin-avatar-icon`]: !!icon,
+      [`kylin-avatar-image`]:
+        isImgExist || React.isValidElement(children),
+    },
+    className
+  )
+
+  const sizeStyle: React.CSSProperties =
+    typeof size === 'number'
+      ? {
+          width: size,
+          height: size,
+          lineHeight: `${size}px`,
+          fontSize: icon ? size / 2 : 18,
+        }
+      : {}
+
+  return (
+    <img
+      ref={ref}
+      src={src}
+      alt={alt}
+      draggable={draggable}
+      className={classes}
+      style={{
+        ...sizeStyle,
+        ...(rest as AvatarProps).style,
+      }}
+    />
+  )
+}
+const Avatar = React.forwardRef<
+  HTMLSpanElement,
+  AvatarProps
+>(InternalAvatar as any)
+
+if (process.env.NODE_ENV !== 'production') {
+  Avatar.displayName = 'Avatar'
+}
+
+export default Avatar
