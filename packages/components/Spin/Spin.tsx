beb4915b15e518daa5f8a3995d036e96e779d956
凤之兮原 <1577310596@qq.com>
2023-06-14 21:04:26 +0800

    refactor: spin type

--- packages/components/Spin/Spin.tsx
+++ packages/components/Spin/Spin.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
-import { SpinProps } from './demo'
+import { SpinProps } from './type'
 
 export const Spin: React.ForwardRefRenderFunction<
   HTMLImageElement,
