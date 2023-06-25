e67ed93489269a9b6b40fe5cca3afe1ec5b38dce
凤之兮原 <1577310596@qq.com>
2023-06-25 22:03:34 +0800

    refactor: refactor Image Handler

--- packages/components/Image/helpers/Handler.ts
+++ packages/components/Image/helpers/Handler.ts
@@ -31,6 +31,7 @@ export function GetImageIconsHandler({
   setPreviewVisible,
 }) {
   let count = 0
+  let rotate = 0
   const limitCount = 5
   const handleClick = key => {
     const dom = document.querySelector(
@@ -38,14 +39,13 @@ export function GetImageIconsHandler({
     ) as any
     switch (key) {
       case ImageTools.PLUS:
-        ++count > limitCount && (count = limitCount)
-        transformPreviewSize()
+        transformPreviewSize(formatCount(ImageTools.PLUS))
         break
       case ImageTools.MINUS:
-        --count < 0 && (count = 0)
-        transformPreviewSize()
+        transformPreviewSize(formatCount(ImageTools.MINUS))
         break
       case ImageTools.ROTATE_CCW:
+        transformPreviewSize(count, ++rotate)
         break
       case ImageTools.CLOSE:
         if (setPreviewVisible) {
@@ -56,13 +56,23 @@ export function GetImageIconsHandler({
         break
     }
 
-    function transformPreviewSize() {
+    function formatCount(type: `${ImageTools}`) {
+      type === ImageTools.PLUS
+        ? ++count > limitCount && (count = limitCount)
+        : type === ImageTools.MINUS
+        ? --count < 0 && (count = 0)
+        : count
+
+      return count
+    }
+
+    function transformPreviewSize(count, rotate = 0) {
       if (dom && count <= limitCount) {
         dom.style.transform = `translate3d(${
           0.25 * count
         }px, 0px, 0px) scale3d(${1 + 0.5 * count}, ${
           1 + 0.5 * count
-        }, 1) rotate(0deg)`
+        }, 1) rotate(${rotate * 90}deg)`
       }
     }
   }
