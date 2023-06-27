6ba5c431b7ca561cb0cc8b9e1492787141adbdc9
凤之兮原 <1577310596@qq.com>
2023-06-27 23:32:44 +0800

    remove: remove card tests

--- packages/components/Card/__tests__/Card.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import { Card } from '../Card'
-
-test('render', () => {
-  const { container } = render(<Card>Card</Card>)
-  expect(container.firstChild).toMatchInlineSnapshot()
-})
