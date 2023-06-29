d2071dbf1400a18add624b74b682ec9e4817b30b
凤之兮原 <1577310596@qq.com>
2023-06-29 10:08:28 +0800

    feat: add Badge story

---
+++ packages/stories/Badge.stories.tsx
@@ -0,0 +1,67 @@
+import { Badge } from '@components/Badge'
+import { Space } from '@components/Space'
+import { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  title: 'UI Component/Badge',
+  component: Badge,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  } as any,
+} satisfies Meta<typeof Badge>
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: () => <Badge>123</Badge>,
+}
+
+export const Type: Story = {
+  render: () => (
+    <Space>
+      <Badge type="secondary">Secondary</Badge>
+      <Badge type="success">Success</Badge>
+      <Badge type="warning">Warning</Badge>
+      <Badge type="danger">Danger</Badge>
+      <Badge type="info">Info</Badge>
+    </Space>
+  ),
+}
+
+export const Shape: Story = {
+  render: () => (
+    <Space>
+      <Badge shape="square">default shape</Badge>
+      <Badge shape="circle">circle</Badge>
+      <Badge shape="rounded">rounded</Badge>
+    </Space>
+  ),
+}
+
+export const Size: Story = {
+  render: () => (
+    <Space>
+      <Badge size="small">small</Badge>
+      <Badge size="middle">middle</Badge>
+      <Badge size="large">large</Badge>
+    </Space>
+  ),
+}
+
+export const Gradient: Story = {
+  render: () => (
+    <Space>
+      <Badge gradient="aquamarine">aqua</Badge>
+      <Badge gradient="blue">blue</Badge>
+      <Badge gradient="orange">orange</Badge>
+      <Badge gradient="red">red</Badge>
+    </Space>
+  ),
+}
+
+export const Text: Story = {
+  render: () => <Badge text="text" />,
+}
+
+export default meta
