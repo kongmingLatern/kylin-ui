c9b8ea002baa3e47120a7eabcceb2baa75cc46f5
凤之兮原 <1577310596@qq.com>
2023-07-04 10:36:47 +0800

    chore: add comment for theme common

--- packages/styled-system/src/common/index.ts
+++ packages/styled-system/src/common/index.ts
@@ -1,5 +1,38 @@
 import { theme as Theme } from '../theme'
 
+/**
+ * @description 根据type, gradient, bgColor 获取背景色
+ * @param `type`
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'danger'
+    | 'warning'
+    | 'info'
+ * @param gradient 'aquamarine' | 'orange' | 'red' | 'blue'
+ * @param bgColor  string | null
+ * @returns
+      `type`: {
+        `default`: '#5123f7',
+        `primary`: '#6e0b75',
+        `secondary`: '#006597',
+        `success`: '#36d399',
+        `info`: '#3abff8',
+        `disabled`: '#bdc3c7',
+        `warning`: '#e67e22',
+        `danger`: '#e74c3c',
+      },
+
+      `gradient`: {
+        `aquamarine`:
+          'linear-gradient(45deg, #00d2ff 0%, #3a7bd5 100%)',
+        `blue`: 'linear-gradient(45deg, #00d2ff 0%, #3a7bd5 100%)',
+        `orange`:
+          'linear-gradient(45deg, #f2994a 0%, #f2c94c 100%)',
+        `red`: 'linear-gradient(45deg, #2193b0 0%, #6dd5ed 100%)',
+      },
+
+ */
 export function getBackground({
   type,
   gradient,
@@ -24,6 +57,14 @@ export function getBackground({
   return Theme['type'][type!] ?? Theme['type']['default']
 }
 
+/**
+ * @description 根据size获取 padding
+ * @param size  number | 'small' | 'middle' | 'large'
+ * @returns
+    `small`: '0 0.4rem',
+    `middle`: '0 0.6rem',
+    `large`: '0 0.8rem',
+ */
 export function getPaddingSize(
   size?: 'small' | 'middle' | 'large' | number
 ) {
@@ -36,6 +77,14 @@ export function getPaddingSize(
     : Theme['padding']['middle']
 }
 
+/**
+ * @description 根据size获取宽度
+ * @param size  number | 'small' | 'middle' | 'large'
+ * @returns
+ *  small: '2.4rem',
+    middle: '3.2rem',
+    large: '4rem',
+ */
 export function getWidthSize(
   size?: 'small' | 'middle' | 'large' | number
 ) {
@@ -48,6 +97,14 @@ export function getWidthSize(
     : Theme['width']['middle']
 }
 
+/**
+ * @description 根据 shape 获取 radius
+ * @param shape  'circle' | 'rounded' | 'square'
+ * @returns
+    `square`: 0,
+    `rounded`: '0.25rem',
+    `circle`: '50%',
+ */
 export function getRadius(
   shape?: 'circle' | 'rounded' | 'square'
 ) {
@@ -59,7 +116,14 @@ export function getRadius(
 export function getMargin({ position }) {
   return position === 'before' ? '0 4px 0 0' : '0 0 0 4px'
 }
-
+/**
+ * @description 根据size获取 fontSize
+ * @param size  number | 'small' | 'middle' | 'large'
+ * @returns
+    `small`: '1rem',
+    `middle`: '1.2rem',
+    `large`: '1.4rem',
+ */
 export function getFontSize(
   size?: 'small' | 'middle' | 'large' | number
 ) {
@@ -72,6 +136,13 @@ export function getFontSize(
     : Theme['fontSize']['middle']
 }
 
+/**
+ * @description 根据 offset 获取 偏移量
+ * @param offset number | string
+ * @returns
+ *  number: `${offset}px`
+ *  string: offset
+ */
 export function getAddSuffixOffset(
   offset: number | string
 ) {
