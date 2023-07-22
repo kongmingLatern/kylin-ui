6a137c1e8d135f0d3c97d5a4ce0c1c151b77a675
凤之兮原 <1577310596@qq.com>
2023-07-22 16:44:45 +0800

    feat: add demo for col stories

--- packages/stories/Col.stories.tsx
+++ packages/stories/Col.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Col, Row } from '@components/Grid'
 import { Space } from '@components/Space'
+import 'uno.css'
 
 const meta = {
   title: 'UI Component/Col',
@@ -10,6 +11,13 @@ const meta = {
 
 type Story = StoryObj<typeof meta>
 
+const basicStyle = {
+  bg: 'blue-300',
+  h: '50px',
+  lh: '50px',
+  text: 'center white',
+}
+
 export const Basic: Story = {
   render: args => <Col>There are some text</Col>,
 }
@@ -18,19 +26,43 @@ export const Flex: Story = {
   render: args => (
     <Space direction="vertical">
       <Row>
-        <Col flex={'0 0 10%'} bg="blue-300" h="50px">
+        <Col
+          flex={'0 0 10%'}
+          bg="blue-300"
+          h="50px"
+          lh="50px"
+          text="center white"
+        >
           0 0 10%
         </Col>
-        <Col flex={'1 1 auto'} bg="blue-400" h="50px">
+        <Col
+          flex={'1 1 auto'}
+          bg="blue-400"
+          h="50px"
+          lh="50px"
+          text="center white"
+        >
           1 1 auto
         </Col>
       </Row>
 
       <Row>
-        <Col flex={1} bg="blue-300" h="50px">
+        <Col
+          flex={1}
+          bg="blue-300"
+          h="50px"
+          lh="50px"
+          text="center white"
+        >
           flex: 1
         </Col>
-        <Col flex={2} bg="blue-400" h="50px">
+        <Col
+          flex={2}
+          bg="blue-400"
+          h="50px"
+          lh="50px"
+          text="center white"
+        >
           flex: 2
         </Col>
       </Row>
@@ -41,17 +73,71 @@ export const Flex: Story = {
 export const Span: Story = {
   render: args => (
     <>
-      <Row>
-        <Col span={2} bg="blue-300" h="50px">
-          2
+      <Row text="white">
+        <Col
+          span={2}
+          bg="blue-300"
+          h="50px"
+          lh="50px"
+          text="center"
+        >
+          col-2
         </Col>
-        <Col span={20} bg="blue-500" h="50px">
-          20
+        <Col
+          span={20}
+          bg="blue-500"
+          h="50px"
+          lh="50px"
+          text="center"
+        >
+          col-20
         </Col>
-        <Col span={2} bg="blue-300" h="50px">
-          2
+        <Col
+          span={2}
+          bg="blue-300"
+          h="50px"
+          lh="50px"
+          text="center"
+        >
+          col-2
         </Col>
       </Row>
+      <Row text="white">
+        {new Array(4).fill(0).map((i, index) => (
+          <Col
+            key={index}
+            span={6}
+            {...basicStyle}
+            bg="blue-400"
+          >
+            col-6
+          </Col>
+        ))}
+      </Row>
+      <Row>
+        {new Array(6).fill(0).map(i => (
+          <Col
+            span={4}
+            {...basicStyle}
+            key={i}
+            bg="blue-600"
+          >
+            4
+          </Col>
+        ))}
+      </Row>
+      <Row>
+        {new Array(8).fill(0).map(i => (
+          <Col
+            span={3}
+            {...basicStyle}
+            key={i}
+            bg="blue-400"
+          >
+            3
+          </Col>
+        ))}
+      </Row>{' '}
     </>
   ),
 }
