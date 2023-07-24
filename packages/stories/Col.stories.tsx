fb6a03b86c40128eb9a42a5b8edd85f88c36cb0f
凤之兮原 <1577310596@qq.com>
2023-07-24 10:42:50 +0800

    feat: add justify and align stories for Grid

--- packages/stories/Col.stories.tsx
+++ packages/stories/Col.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Col, Row } from '@components/Grid'
 import { Space } from '@components/Space'
 import 'uno.css'
+import { Divider } from '@components/Divider'
+import { Fragment } from 'react'
 
 const meta = {
   title: 'UI Component/Col',
@@ -16,10 +18,6 @@ const basicStyle = {
   h: '50px',
   lh: '50px',
   text: 'center white',
-}
-const style: React.CSSProperties = {
-  background: '#0092ff',
-  padding: '8px 0',
 }
 
 export const Basic: Story = {
@@ -145,6 +143,179 @@ export const Gutter: Story = {
         </Col>
       ))}
     </Row>
+  ),
+}
+
+export const Offset: Story = {
+  render: args => (
+    <Row gutter={[0, 24]}>
+      <Col span={8} {...basicStyle} bg="blue-500">
+        col-6
+      </Col>
+      <Col
+        span={8}
+        offset={8}
+        {...basicStyle}
+        bg="blue-500"
+      >
+        col-6
+      </Col>
+    </Row>
+  ),
+}
+
+export const Justify: Story = {
+  render: args => (
+    <Space direction="vertical">
+      <Divider>start</Divider>
+      <Row justify="start">
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+      </Row>
+      <Divider>center</Divider>
+      <Row justify="center">
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+      </Row>
+      <Divider>end</Divider>
+      <Row justify="end">
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+      </Row>
+      <Divider>space-between</Divider>
+      <Row justify="space-between">
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+      </Row>
+      <Divider>space-around</Divider>
+      <Row justify="space-around">
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+      </Row>
+      <Divider>space-evently</Divider>
+      <Row justify="space-evenly">
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+        <Col span={4} {...basicStyle} bg="blue-500">
+          col-4
+        </Col>
+      </Row>
+    </Space>
+  ),
+}
+
+export const Align: Story = {
+  render: args => (
+    <Space direction="vertical">
+      <Divider>start</Divider>
+      <Row
+        justify="center"
+        align="start"
+        style={{ height: 100 }}
+      >
+        <Col
+          span={4}
+          bg="blue-500"
+          h="100px"
+          lh="100px"
+          text="center white"
+        >
+          col-4
+        </Col>
+        <Col span={4} {...basicStyle} bg="blue-300">
+          col-4
+        </Col>
+        <Col
+          span={4}
+          bg="blue-500"
+          h="100px"
+          lh="100px"
+          text="center white"
+        >
+          col-4
+        </Col>
+        <Col span={4} {...basicStyle} bg="blue-300">
+          col-4
+        </Col>
+      </Row>
+      <Divider>center</Divider>
+      <Row
+        justify="center"
+        align="center"
+        style={{ height: 100 }}
+      >
+        {new Array(2).fill(0).map((i, k) => {
+          return (
+            <Fragment key={k}>
+              <Col
+                span={4}
+                {...basicStyle}
+                h="80px"
+                lh="80px"
+                bg={'blue-500'}
+              >
+                col-4
+              </Col>
+              <Col span={4} {...basicStyle} bg={'blue-400'}>
+                col-4
+              </Col>
+            </Fragment>
+          )
+        })}
+      </Row>
+      <Divider>end</Divider>
+      <Row
+        justify="center"
+        align="end"
+        style={{ height: 100 }}
+      >
+        {new Array(2).fill(0).map((i, k) => {
+          return (
+            <Fragment key={k}>
+              <Col
+                span={4}
+                {...basicStyle}
+                h="80px"
+                lh="80px"
+                bg={'blue-500'}
+              >
+                col-4
+              </Col>
+              <Col span={4} {...basicStyle} bg={'blue-400'}>
+                col-4
+              </Col>
+            </Fragment>
+          )
+        })}
+      </Row>
+    </Space>
   ),
 }
 
