a80943e29c3a3ba440afc870e0e93ff8895a297c
凤之兮原 <1577310596@qq.com>
2023-08-02 09:29:30 +0800

    chore: add gloabl type

--- packages/shared/src/type/global.d.ts
+++ packages/shared/src/type/global.d.ts
@@ -1,3 +1,13 @@
 type SizeType = 'small' | 'middle' | 'large' | number
 
 type ShapeType = 'circle' | 'rounded' | 'square'
+
+type ThemeType =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'danger'
+  | 'warning'
+  | 'info'
+  | 'error'
+  | 'disabled'
