0b46f25d17293663400ee6e3758851f7c9210fd6
凤之兮原 <1577310596@qq.com>
2023-08-19 08:49:18 +0800

    refactor: refactor List story

--- packages/stories/List.stories.tsx
+++ packages/stories/List.stories.tsx
@@ -10,80 +10,77 @@ const meta = {
 } satisfies Meta<typeof List>
 
 type Story = StoryObj<typeof meta>
+const dataSource = [
+  {
+    avatar: <Avatar src="https://i.pravatar.cc/300" />,
+    title: 'Title',
+    description:
+      'De123123123123123123123123123123123123333333333333333333scription',
+    extra: <a>extra</a>,
+    actions: [
+      <span
+        key={1}
+        style={{
+          display: 'flex',
+          width: '100%',
+        }}
+      >
+        <Heart width={15} height={15} color="gray" />
+        <span
+          style={{
+            marginLeft: '5px',
+            fontSize: '.5rem',
+          }}
+        >
+          Like
+        </span>
+      </span>,
+      <span
+        key={2}
+        style={{
+          display: 'flex',
+          width: '100%',
+        }}
+      >
+        <Star width={15} height={15} color="gray" />
+        <span
+          style={{
+            marginLeft: '5px',
+            fontSize: '.5rem',
+          }}
+        >
+          Star
+        </span>
+      </span>,
+      <span
+        key={3}
+        style={{
+          display: 'flex',
+          width: '100%',
+        }}
+      >
+        <MessageCircle
+          width={15}
+          height={15}
+          color="gray"
+        />
+        <span
+          style={{
+            marginLeft: '5px',
+            fontSize: '.5rem',
+          }}
+        >
+          Comment
+        </span>
+      </span>,
+    ],
+  },
+]
 
 export const Basic: Story = {
-  render: () => (
-    <List
-      dataSource={[
-        {
-          avatar: (
-            <Avatar src="https://i.pravatar.cc/300" />
-          ),
-          title: 'Title',
-          description:
-            'De123123123123123123123123123123123123333333333333333333scription',
-          extra: <a>extra</a>,
-          actions: [
-            <span
-              key={1}
-              style={{
-                display: 'flex',
-                width: '100%',
-              }}
-            >
-              <Heart width={15} height={15} color="gray" />
-              <span
-                style={{
-                  marginLeft: '5px',
-                  fontSize: '.5rem',
-                }}
-              >
-                Like
-              </span>
-            </span>,
-            <span
-              key={2}
-              style={{
-                display: 'flex',
-                width: '100%',
-              }}
-            >
-              <Star width={15} height={15} color="gray" />
-              <span
-                style={{
-                  marginLeft: '5px',
-                  fontSize: '.5rem',
-                }}
-              >
-                Star
-              </span>
-            </span>,
-            <span
-              key={3}
-              style={{
-                display: 'flex',
-                width: '100%',
-              }}
-            >
-              <MessageCircle
-                width={15}
-                height={15}
-                color="gray"
-              />
-              <span
-                style={{
-                  marginLeft: '5px',
-                  fontSize: '.5rem',
-                }}
-              >
-                Comment
-              </span>
-            </span>,
-          ],
-        },
-      ]}
-    ></List>
-  ),
+  render: () => {
+    return <List dataSource={dataSource} />
+  },
 }
 
 export default meta
